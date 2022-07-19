8cc32ecdc8a3d2971331f166cfc3b81b57bef840
이소윤 <s_lee77176@fanshaweonline.ca>
2022-07-19 00:43:08 -0700

    trying with chrome

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-const { Builder } = require('selenium-webdriver');
+const { Builder, By, Key, until } = require('selenium-webdriver');
 (async function example() {
     let driver = await new Builder()
     .forBrowser('chrome')
     .build();
     try {
-        // 네이버 실행
-        await driver.get('https://www.naver.com/');
+        
+        await driver.get('https://www.google.com');
+        // let userAgent = await driver.executeScript("return navigator.userAgent;")
+        // console.log('[UserAgent]', userAgent);
+        let input = await driver.findElement(By.className('gLFyf'));
+
+        let keyword = 'South Korea';
+        input.sendKeys(keyword, Key.ENTER);
+
+        await driver.wait(until.elementLocated(By.css('.SDkEP')), 10000);
+        let resultElements = await driver.findElement(By.className('yuRUbf'));
+        console.log('[resultElements.length]', resultElements.length);
+
+        try {
+            await driver.wait(() => { return false; }, 4000);
+        } catch (err) {
+
+        }  
+        
     }
     finally{
         driver.quit();
